948d27b61080473b468d2b8af4c50591c40e6e5b
Allen, Timothy <tallen@wharton.upenn.edu>
2019-08-10 17:10:08 -0400

    Get backend integrated so it will successfully allow runserver and initial migration against SQL Server 2016.

--- django/db/backends/mssql/base.py
+++ django/db/backends/mssql/base.py
@@ -6,12 +6,6 @@ import re
 import time
 
 from django.core.exceptions import ImproperlyConfigured
-from django import VERSION
-
-if VERSION[:3] < (2, 2, 0) or VERSION[:2] >= (2, 3):
-    raise ImproperlyConfigured(
-        "Django %d.%d.%d is not supported." % VERSION[:3]
-    )
 
 try:
     import pyodbc as Database
